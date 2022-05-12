113f041a4acf7390c33426a276c10bcd4372a970
Pierre Ribault <contact@pierreribault.fr>
2022-05-12 12:19:48 +0200

    Introduce login route

--- composeexample/urls.py
+++ composeexample/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('consulting/', include('consulting.urls')),
+    path('', include('consulting.urls')),
 ]
 
