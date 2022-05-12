ba222c66e57ee1a91f9ffb74403d32f06dc7e28f
Pierre Ribault <contact@pierreribault.fr>
2022-05-12 10:54:42 +0200

    Introduce Consulting

--- composeexample/settings.py
+++ composeexample/settings.py
@@ -32,6 +32,7 @@ ALLOWED_HOSTS = []
 # Application definition
 
 INSTALLED_APPS = [
+    'consulting.apps.ConsultingConfig',
     'django.contrib.admin',
     'django.contrib.auth',
     'django.contrib.contenttypes',
