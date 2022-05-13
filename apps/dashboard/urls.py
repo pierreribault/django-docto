49347cc221c3a0c1e3326aa20f4630167e05c891
Loan Besson <loan.besson@hotmail.fr>
2022-05-13 10:25:15 +0200

    Start agenda

--- apps/dashboard/urls.py
+++ apps/dashboard/urls.py
@@ -12,6 +12,10 @@ urlpatterns = [
     path('slot', views.slot, name='slot'),
     path('slot/new', views.slot_new, name='slot_new'),
     path('slot/<int:slot_id>/delete', views.slot_delete, name='slot_delete'),
+    path('service', views.service, name='service'),
+    path('service/new', views.service_new, name='service_new'),
+    path('service/<int:service_id>/delete', views.service_delete, name='service_delete'),
+    path('calendar', views.calendar, name='calendar'),
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
