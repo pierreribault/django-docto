e8d3166808ccbf42d8d4f480329372a9d3a43b9d
Gueguen Dorian <doriangueguen@orange.fr>
2022-05-13 11:22:08 +0200

    refacto template creation cabinet

--- apps/authentication/forms.py
+++ apps/authentication/forms.py
@@ -9,14 +9,14 @@ class LoginForm(forms.Form):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
+                "placeholder": "Nom d'utilisateur",
                 "class": "form-control"
             }
         ))
     password = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password",
+                "placeholder": "Mot de passe",
                 "class": "form-control"
             }
         ))
@@ -26,7 +26,7 @@ class SignUpForm(UserCreationForm):
     username = forms.CharField(
         widget=forms.TextInput(
             attrs={
-                "placeholder": "Username",
+                "placeholder": "Nom d'utilisateur",
                 "class": "form-control"
             }
         ))
@@ -40,14 +40,14 @@ class SignUpForm(UserCreationForm):
     password1 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password",
+                "placeholder": "Mot de passe",
                 "class": "form-control"
             }
         ))
     password2 = forms.CharField(
         widget=forms.PasswordInput(
             attrs={
-                "placeholder": "Password check",
+                "placeholder": "Confirmation du mot de passe",
                 "class": "form-control"
             }
         ))
